2e404bf4fbc4b5ff1b0991f1d4b9017a91b48d1a
adamsm6 <adamsm6@rpi.edu>
2018-03-22 16:30:11 -0400

    Updated table columns to hold percent change data and enabled collect script to run via a management command cron job

--- prophet/coinData.py
+++ prophet/coinData.py
@@ -1,10 +1,11 @@
 from prophet import models
+import datetime
 import json
 import requests
 
 
 # Save data returned from CoinMarketCap
-class DataModel():
+class CoinData():
     data = {}
 
     # Store response from call to coin market cap
@@ -26,7 +27,7 @@ class Data():
 
 
 # Adapt data retrieved from CoinMarketCap to store in database
-class DataAdapter(Data):
+class CoinDataAdapter(Data):
 
     def __init__(self, data_model):
         self.my_data = data_model
@@ -34,28 +35,43 @@ class DataAdapter(Data):
     # For a coin we save the coin's information in the
     # historical data table and current data table
     def save_indiv(self, i):
-        self.name = self.my_data.get("name", i)
-        self.datetime = int(self.my_data.get("last_updated", i))
-        self.historical_price = float(self.my_data.get("price_usd", i))
-        self.market_cap = float(self.my_data.get("market_cap_usd", i))
         self.circulating_supply = float(
                 self.my_data.get("available_supply", i))
+        self.datetime = datetime.datetime.fromtimestamp(
+                int(self.my_data.get("last_updated", i)))
+        self.historical_price = float(self.my_data.get("price_usd", i))
+        self.market_cap = float(self.my_data.get("market_cap_usd", i))
+        self.name = self.my_data.get("name", i)
         self.volume = float(self.my_data.get("total_supply", i))
 
+        # Update historical data table
         historical_data = models.Historical(
-                name=self.name,
+                circulating_supply=self.circulating_supply,
                 datetime=self.datetime,
                 historical_price=self.historical_price,
                 market_cap=self.market_cap,
-                circulating_supply=self.circulating_supply,
+                name=self.name,
                 volume=self.volume)
+
         historical_data.save()
 
+        # Update current coin data table
+        self.price_change_day = float(
+                self.my_data.get("percent_change_24h", i))
+        self.price_change_hour = float(
+                self.my_data.get("percent_change_1h", i))
+        self.price_change_week = float(
+                self.my_data.get("percent_change_7d", i))
+        self.rank = self.my_data.get("rank", i)
         self.symbol = self.my_data.get("symbol", i)
         updates = {"circulating_supply": self.circulating_supply,
-                   "market_cap": self.circulating_supply,
+                   "market_cap": self.market_cap,
                    "name": self.name,
                    "price": self.historical_price,
+                   "price_change_day": self.price_change_day,
+                   "price_change_hour": self.price_change_hour,
+                   "price_change_week": self.price_change_week,
+                   "rank": self.rank,
                    "volume": self.volume}
         models.Coin.objects.update_or_create(symbol=self.symbol,
                                              defaults=updates)
--- prophet/models.py
+++ prophet/models.py
@@ -8,8 +8,9 @@ import datetime
 class Historical(models.Model):
     circulating_supply = models.DecimalField(max_digits=19, decimal_places=2,
                                              default=0)
-    datetime = models.IntegerField(default=0)
-    historical_price = models.DecimalField(max_digits=19, decimal_places=2,
+    datetime = models.DateTimeField(default=datetime.datetime.now(),
+                                    blank=True)
+    historical_price = models.DecimalField(max_digits=19, decimal_places=10,
                                            default=0)
     market_cap = models.DecimalField(max_digits=19, decimal_places=2,
                                      default=0)
@@ -17,11 +18,15 @@ class Historical(models.Model):
     volume = models.DecimalField(max_digits=19, decimal_places=2,
                                  default=0)
 
+    class Meta:
+        unique_together = ('name', 'datetime',)
+
 
 class Prediction(models.Model):
-    datetime = models.IntegerField(default=0)
+    datetime = models.DateTimeField(default=datetime.datetime.now(),
+                                    blank=True)
     name = models.CharField(max_length=30, default="")
-    predicted_price = models.DecimalField(max_digits=19, decimal_places=2)
+    predicted_price = models.DecimalField(max_digits=19, decimal_places=10)
 
 
 class Coin(models.Model):
@@ -29,7 +34,14 @@ class Coin(models.Model):
                                              decimal_places=2, default=0)
     market_cap = models.DecimalField(max_digits=19,
                                      decimal_places=2, default=0)
-    name = models.CharField(max_length=30, default="")
-    price = models.DecimalField(max_digits=19, decimal_places=2, default=0)
+    name = models.CharField(primary_key=True, max_length=30, default="")
+    price = models.DecimalField(max_digits=19, decimal_places=10, default=0)
+    rank = models.IntegerField(default=0)
     symbol = models.CharField(max_length=6, default="")
+    price_change_day = models.DecimalField(max_digits=19,
+                                           decimal_places=2, default=0)
+    price_change_hour = models.DecimalField(max_digits=19,
+                                            decimal_places=2, default=0)
+    price_change_week = models.DecimalField(max_digits=19,
+                                            decimal_places=2, default=0)
     volume = models.DecimalField(max_digits=19, decimal_places=2, default=0)
