ed044bf650434baa5cd27ba1e1f06a91ab04a6ff
Andy Wu <wua6@rpi.edu>
2018-04-25 16:30:02 -0400

    fixed more syntax errors

--- config/settings/production.py
+++ config/settings/production.py
@@ -135,7 +135,7 @@ ANYMAIL = {
     'EMAIL_HOST': env('MAILGUN_SMTP_SERVER', ''),
     'EMAIL_PORT': env('MAILGUN_SMTP_PORT', ''),
     'EMAIL_HOST_USER': env('MAILGUN_SMTP_LOGIN', ''),
-    'EMAIL_HOST_PASSWORD': = env('MAILGUN_SMTP_PASSWORD', '')
+    'EMAIL_HOST_PASSWORD': env('MAILGUN_SMTP_PASSWORD', '')
 }
 # EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
